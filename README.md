c15271dc0337908fa71acd7b02126ac4853a471f
Bradley Diep <bdippin12@csu.fullerton.edu>
2022-03-31 14:07:08 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Super-Mario
+CPSC 386-02 Project 2
+
+# How to Play
+* press LEFT/RIGHT/DOWN key to control Mario
+* press key 'a' to jump
+* press key 's' to shoot fireball or run
--- settings.py
+++
@@ -1,7 +0,0 @@
-from vector import Vector
-
-class Settings:
-    def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = 102, 178, 255
